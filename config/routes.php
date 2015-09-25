3e196d50a7b5860d388aae86505879becccd67fa
jeffblack360 <jeffblack360@gmail.com>
2015-09-24 21:09:15 -0500

    Completed part 1 of How to build a CakePHP 3 REST API in minutes

--- config/routes.php
+++ config/routes.php
@@ -41,7 +41,11 @@ use Cake\Routing\Router;
  */
 Router::defaultRouteClass('DashedRoute');
 
+Router::extensions(['json', 'xml']);
+
 Router::scope('/', function ($routes) {
+    $routes->resources('Films');
+    
     /**
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
      * its action called 'display', and we pass a param to select the view file
